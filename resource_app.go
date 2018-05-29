ef962f584abd9fe9167895a5987db0df85f1a348
Derek Steinmoeller <Derek.Steinmoeller@D2L.com>
2018-05-29 13:13:02 -0400

    Put identity stuff on a composed struct.

--- resource_app.go
+++ resource_app.go
@@ -11,22 +11,24 @@ type Settings struct {
 }
 
 type Application struct {
-	ID          string      `json:"id"`
-	Name        string      `json:"name"`
-	Label       string      `json:"label"`
-	SignOnMode  string      `json:"signOnMode"`
-	Settings    Settings    `json:"settings"`
-	Credentials Credentials `json:"credentials"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Label      string `json:"label"`
+	SignOnMode string `json:"signOnMode"`
 }
 
 type AppSettings struct {
-	AwsEnvironmentType  string `json:"awsEnvironmentType"`
-	GroupFilter         string `json:"groupFilter"`
-	LoginURL            string `json:"loginUrl"`
-	JoinAllRoles        bool   `json:"joinAllRoles"`
-	SessionDuration     int    `json:"sessionDuration"`
-	RoleValuePattern    string `json:"roleValuePattern"`
-	IdentityProviderArn string `json:"identityProviderArn"`
+	AwsEnvironmentType string `json:"awsEnvironmentType"`
+	GroupFilter        string `json:"groupFilter"`
+	LoginURL           string `json:"loginUrl"`
+	JoinAllRoles       bool   `json:"joinAllRoles"`
+	SessionDuration    int    `json:"sessionDuration"`
+	RoleValuePattern   string `json:"roleValuePattern"`
+}
+
+type IdentifiedApplication struct {
+	Application
+	Credentials Credentials `json:"credentials"`
 }
 
 type Credentials struct {
@@ -106,13 +108,12 @@ func resourceAppCreate(d *schema.ResourceData, m interface{}) error {
 		SignOnMode: d.Get("sign_on_mode").(string),
 		Settings: Settings{
 			App: AppSettings{
-				AwsEnvironmentType:  d.Get("aws_environment_type").(string),
-				GroupFilter:         d.Get("group_filter").(string),
-				LoginURL:            d.Get("login_url").(string),
-				JoinAllRoles:        d.Get("join_all_roles").(bool),
-				SessionDuration:     d.Get("session_duration").(int),
-				RoleValuePattern:    d.Get("role_value_pattern").(string),
-				IdentityProviderArn: d.Get("identity_provider_arn").(string),
+				AwsEnvironmentType: d.Get("aws_environment_type").(string),
+				GroupFilter:        d.Get("group_filter").(string),
+				LoginURL:           d.Get("login_url").(string),
+				JoinAllRoles:       d.Get("join_all_roles").(bool),
+				SessionDuration:    d.Get("session_duration").(int),
+				RoleValuePattern:   d.Get("role_value_pattern").(string),
 			},
 		},
 	}
@@ -156,13 +157,12 @@ func resourceAppUpdate(d *schema.ResourceData, m interface{}) error {
 		SignOnMode: d.Get("sign_on_mode").(string),
 		Settings: Settings{
 			App: AppSettings{
-				AwsEnvironmentType:  d.Get("aws_environment_type").(string),
-				GroupFilter:         d.Get("group_filter").(string),
-				LoginURL:            d.Get("login_url").(string),
-				JoinAllRoles:        d.Get("join_all_roles").(bool),
-				SessionDuration:     d.Get("session_duration").(int),
-				RoleValuePattern:    d.Get("role_value_pattern").(string),
-				IdentityProviderArn: d.Get("identity_provider_arn").(string),
+				AwsEnvironmentType: d.Get("aws_environment_type").(string),
+				GroupFilter:        d.Get("group_filter").(string),
+				LoginURL:           d.Get("login_url").(string),
+				JoinAllRoles:       d.Get("join_all_roles").(bool),
+				SessionDuration:    d.Get("session_duration").(int),
+				RoleValuePattern:   d.Get("role_value_pattern").(string),
 			},
 		},
 	}
