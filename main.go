b27c61628e0828d6a1761ce627c4fbdedcd9e32a
jrbeverly <Jonathan.Beverly@D2L.com>
2019-02-05 14:11:02 -0500

    Match main to fit evident provider

--- main.go
+++ main.go
@@ -6,9 +6,5 @@ import (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
-		},
-	})
+	plugin.Serve(&plugin.ServeOpts{ProviderFunc: okta.Provider})
 }
