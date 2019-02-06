ec7a7956bb9df6d7c75315079a3d9dc3e7b6d64d
jrbeverly <Jonathan.Beverly@D2L.com>
2019-02-06 17:04:21 -0500

    Configure terraform provider with example

---
+++ examples/appusers/main.tf
@@ -0,0 +1,19 @@
+provider "okta" {
+  okta_url       = "https://dev-750049.oktapreview.com"
+  okta_admin_url = "https://dev-750049-admin.oktapreview.com"
+  api_key        = "${var.okta_api_key}"
+  username       = "${var.okta_username}"
+  password       = "${var.okta_password}"
+  org_id         = "0oaf1twj0t8kgcCs60h7"
+}
+
+variable "okta_api_key" {}
+variable "okta_username" {}
+variable "okta_password" {}
+
+resource "okta_user_attachment" "readonly" {
+  app_id     = "0oaiquchfr6AoYiqB0h7"
+  user       = "00ufr7ysvmSTCCW9Z0h7"
+  role       = "ACE-Edgeville-Owner"
+  saml_roles = ["ACE-Edgeville-Owner"]
+}
