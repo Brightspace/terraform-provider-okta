f992ea7d7af32cca79c61d68b8b1bff07d4f49ba
jrbeverly <Jonathan.Beverly@D2L.com>
2019-02-07 10:32:49 -0500

    Configure resource with array of users and roles

--- examples/appusers/main.tf
+++ examples/appusers/main.tf
@@ -7,13 +7,26 @@ provider "okta" {
   org_id         = "0oaf1twj0t8kgcCs60h7"
 }
 
+locals {
+  users = {
+    "Jonathan.Beverly@d2l.com" = "ACE-Edgeville-Owner",
+    "Edvard.Kikic@d2l.com" = "ACE-Edgeville-User,ACE-Edgeville-ReadOnly",
+    "sean.cowing@d2l.com" = "ACE-Edgeville-User",
+    "Chris.Stavropoulos@d2l.com" = "ACE-Edgeville-User",
+    "Derek.Steinmoeller@d2l.com" = "ACE-Edgeville-ReadOnly",
+  }
+
+  emails = "${keys(local.users)}"
+}
+
 variable "okta_api_key" {}
 variable "okta_username" {}
 variable "okta_password" {}
 
 resource "okta_user_attachment" "readonly" {
+  count      = "${length(local.emails)}"
   app_id     = "0oaiquchfr6AoYiqB0h7"
-  user       = "Jonathan.Beverly@d2l.com"
+  user       = "${element(local.emails, count.index)}"
   role       = "ACE-Edgeville-Owner"
-  saml_roles = ["ACE-Edgeville-Owner", "ACE-Edgeville-ReadOnly"]
+  saml_roles = "${sort(split(",", lookup(local.users, element(local.emails, count.index), "")))}"
 }
