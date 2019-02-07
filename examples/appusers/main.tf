ddb67608fcc9ccc674f4b7fab6a8510ea10de98b
jrbeverly <Jonathan.Beverly@D2L.com>
2019-02-07 09:45:38 -0500

    Use email instead of userid, add in second role

--- examples/appusers/main.tf
+++ examples/appusers/main.tf
@@ -13,7 +13,7 @@ variable "okta_password" {}
 
 resource "okta_user_attachment" "readonly" {
   app_id     = "0oaiquchfr6AoYiqB0h7"
-  user       = "00ufr7ysvmSTCCW9Z0h7"
+  user       = "Jonathan.Beverly@d2l.com"
   role       = "ACE-Edgeville-Owner"
-  saml_roles = ["ACE-Edgeville-Owner"]
+  saml_roles = ["ACE-Edgeville-Owner", "ACE-Edgeville-ReadOnly"]
 }
