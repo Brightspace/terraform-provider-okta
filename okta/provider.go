b06ad6ede9423a9c627eabf62341708168d0006b
Jonathan Beverly <Jonathan.Beverly@D2L.com>
2020-01-21 11:15:15 -0500

    Remove workaround for terraform-11

--- okta/provider.go
+++ okta/provider.go
@@ -49,9 +49,6 @@ func Provider() terraform.ResourceProvider {
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"okta_app":                     resourceApp(),
-			"okta_user_attachment":         resourceAppUserAttachment(),
-			"okta_app_users":               resourceAppUsers(),
 			"okta_app_aws":                 resourceAppAws(),
 			"okta_app_aws_provision":       resourceAppAwsProvision(),
 			"okta_app_aws_user_attachment": resourceAppUserAttachment(),
